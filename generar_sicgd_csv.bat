c1d5c2c77621b4a6df8ca0fefeb7e26935d150b9
ricardo <rcvaldes@gmail.com>
2025-01-21 17:30:58 -0600

    Create generar_sicgd_csv.bat

---
+++ generar_sicgd_csv.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Elaborador de Reportes SICGD de archivos de base de datos
+
+:: El primer argumento es el año de búsqueda
+if "%1" == "" (
+    set "anio=%date:~6,4%"
+) else (
+    set "anio=%1"
+)
+
+:: El segundo argumento es el mes de búsqueda
+if "%2" == "" (
+    set "mes=%date:~3,2%"
+) else (
+    set "mes=%2"
+)
+echo La fecha asignada es: %anio%-%mes%
+
+:: Crear archivo de reporte
+echo Generando reporte
+set "nombre_archivo=reporte_%anio%%mes%.csv"
+echo Fecha,"Nombre Archivo","Tamanio (MB)" > %nombre_archivo%
+
+:: Listar archivos con la fecha de búsqueda en su nombre
+(
+    for /f "tokens=*" %%a in ('dir /b /s *.BAK') do (
+    set "tamanio=%%~za"
+    set /a "tamanioMB=!tamanio! / 1024 / 1024"
+        echo %%~nxa,!tamanioMB!
+    )
+) >> %nombre_archivo%
+
+:: Fin de ejecucion
+pause exit
